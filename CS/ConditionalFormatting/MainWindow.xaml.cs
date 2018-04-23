f12854d1b19b787fef8e7f2ef203e38ea0868ff1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ConditionalFormatting/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ConditionalFormatting {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
